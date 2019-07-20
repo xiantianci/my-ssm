67a0e1529f24af175ce9dcb54fa8b1a8dec2827c
xiantianci <1580631759@qq.com>
2019-07-20 10:41:30 +0800

    第一次处理

--- TestSpringMvc/src/com/bjsxt/mapper/MenuMapper.java
+++
@@ -1,9 +0,0 @@
-package com.bjsxt.mapper;
-
-import java.util.List;
-
-import com.bjsxt.pojo.Menu;
-
-public interface MenuMapper {
-	List<Menu> selectByPid(int pid);
-}
--- TestSpringMvc/src/com/bjsxt/service/MenuService.java
+++
@@ -1,9 +0,0 @@
-package com.bjsxt.service;
-
-import java.util.List;
-
-import com.bjsxt.pojo.Menu;
-
-public interface MenuService {
-	List<Menu> show();
-}
